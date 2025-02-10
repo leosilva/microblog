382ddcbea15a3bc48f75970a368d26471aba79bb
Leo Moreira Silva <leo.moreira@me.com>
2025-02-10 10:52:01 -0300

    adding the new migration script including posts database

---
+++ app/models/post.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+from .user import User
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
