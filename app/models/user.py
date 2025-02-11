42351bd07fc682daea67fa76127ed56b7a5e1567
Leo Moreira Silva <leo.moreira@me.com>
2025-02-11 11:02:14 -0300

    creating the login function

--- app/models/user.py
+++ app/models/user.py
@@ -2,10 +2,12 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
-# from .post import Post
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -14,3 +16,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(self, user_id):
+        return db.session.get(User, int(user_id))
